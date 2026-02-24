c080e292605544273c970162da465cd7f7716234
freyes <freyes@udel.edu>
2026-02-24 17:01:33 -0500

    Fixed bug with verbose option for integrators

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -1,7 +1,7 @@
 function evolve1D(s0, tspan, p::LLGParams1D; reltol = 1e-6, abstol = 1e-6, verbose=false)
     # A callback function could be used to include time dependent parameters
     cb = DE.DiscreteCallback((u,t,integrator)->true,
-                      integrator->normalize_spins1D!(integrator.u, integrator.p, integrator.t, verbose))
+                      integrator->normalize_spins1D!(integrator.u, integrator.p, integrator.t, verbose=verbose))
 
     prob = DE.ODEProblem(rhs1D_DE!, reshape(s0, 3*p.Nx), tspan, p)
     sol = DE.solve(prob, DE.Tsit5(), reltol = reltol, abstol = abstol, callback=cb)
@@ -10,7 +10,7 @@ end
 
 function evolve2D(s0, tspan, p::LLGParams2D; reltol = 1e-6, abstol = 1e-6, verbose=false)
     cb = DE.DiscreteCallback((u,t,integrator)->true,
-                      integrator->normalize_spins2D!(integrator.u, integrator.p, integrator.t, verbose))
+                      integrator->normalize_spins2D!(integrator.u, integrator.p, integrator.t, verbose=verbose))
 
     prob = DE.ODEProblem(rhs2D_DE!, reshape(s0, 3*p.Nx*p.Ny), tspan, p)
     sol = DE.solve(prob, DE.Tsit5(), reltol = reltol, abstol = abstol, callback=cb)
